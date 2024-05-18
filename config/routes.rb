23e3c3495ec4b2bb45167fce9b85c36c2a2231c1
vanesa camano <vanesasoledad90@gmail.com>
2024-05-18 20:56:58 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  delete "tasks/:id", to: "tasks#destroy"
+  patch "tasks/:id", to: "tasks#update"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
